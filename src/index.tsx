3fd7379fd0311fcf3820e5aa40a0ed8f82eb4bfe
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-04-17 11:31:37 +0930

    feat: begin React app

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+// Import
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Root Component
+const Root: React.FC = () => {
+	return <h4>Hello, world!</h4>;
+}
+
+// Render to DOM
+ReactDOM.render(<Root />, document.getElementById('root'));
